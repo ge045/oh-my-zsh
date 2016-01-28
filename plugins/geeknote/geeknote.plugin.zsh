a385ba62f4469779211d37a5a0df28788c1aeae9
Georg Ogris <georg.ogris@catalysts.cc>
2016-01-28 22:38:31 +0100

    added more gn* shortcuts

--- plugins/geeknote/geeknote.plugin.zsh
+++ plugins/geeknote/geeknote.plugin.zsh
@@ -10,12 +10,23 @@ alias gn='geeknote'
 
 # add to a new bookmark to Bookmarks
 function gnb() {
-	local link=$1
-	shift 1
+	local note=$1
+	#shift 1
 
-	geeknote create --title $link \
-	       	--content $link \
+	geeknote create --title $note \
+	       	--content $note \
 		--notebook Bookmarks \
 		--tags readings
 		#--tags $@
 }
+
+function gnc() {
+	local note=$1
+	geeknote edit --note $note --content WRITE --notebook catalysts
+}
+
+function gns() {
+	local pattern=$1
+	geeknote find --search $pattern --notebooks catalysts --content-search
+}
+
